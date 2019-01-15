6806b73ec42250200bf6fb4fc36ff0e4bb39be8b
Erol Uysal <eutkuuysal@gmail.com>
2019-01-15 23:59:34 +0300

    first init

---
+++ publish.rb
@@ -0,0 +1,19 @@
+require 'json'
+require 'jwt'
+require 'net/http'
+
+payload = { :mercure => { :publish => [:foo] } };
+token = JWT.encode payload, 'secret', 'HS256', { typ: 'JWT' }
+
+Net::HTTP.start('localhost', 3314) do |http|
+  req = Net::HTTP::Post.new('/hub')
+  req['Authorization'] = "Bearer #{token}"
+  req['Content-Type'] = "Content-type: application/x-www-form-urlencoded"
+  req.form_data = {
+    :topic => "http://localhost:3000/demo/books/1.jsonld",
+    :data => { :key => :value }.to_json
+  }
+  req = http.request(req)
+
+  puts req.body
+end
