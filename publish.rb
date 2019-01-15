985529f5c7f06b2e323afe4f00137823c9017c9c
Erol Uysal <eutkuuysal@gmail.com>
2019-01-16 00:02:57 +0300

    removed unnecessary line

--- publish.rb
+++ publish.rb
@@ -8,7 +8,6 @@ token = JWT.encode payload, 'secret', 'HS256', typ: 'JWT'
 Net::HTTP.start('localhost', 3314) do |http|
   req = Net::HTTP::Post.new('/hub')
   req['Authorization'] = "Bearer #{token}"
-  req['Content-Type'] = 'Content-type: application/x-www-form-urlencoded'
   req.form_data = {
     topic: 'http://localhost:3000/demo/books/1.jsonld',
     data: { key: :value }.to_json
