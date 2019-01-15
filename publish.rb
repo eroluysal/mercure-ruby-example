b6609ae6c2279783e3d64d8c9a79da606be8a8f8
Erol Uysal <eutkuuysal@gmail.com>
2019-01-16 00:00:57 +0300

    rubocop configuration

--- publish.rb
+++ publish.rb
@@ -2,16 +2,16 @@ require 'json'
 require 'jwt'
 require 'net/http'
 
-payload = { :mercure => { :publish => [:foo] } };
-token = JWT.encode payload, 'secret', 'HS256', { typ: 'JWT' }
+payload = { mercure: { publish: [:foo] } }
+token = JWT.encode payload, 'secret', 'HS256', typ: 'JWT'
 
 Net::HTTP.start('localhost', 3314) do |http|
   req = Net::HTTP::Post.new('/hub')
   req['Authorization'] = "Bearer #{token}"
-  req['Content-Type'] = "Content-type: application/x-www-form-urlencoded"
+  req['Content-Type'] = 'Content-type: application/x-www-form-urlencoded'
   req.form_data = {
-    :topic => "http://localhost:3000/demo/books/1.jsonld",
-    :data => { :key => :value }.to_json
+    topic: 'http://localhost:3000/demo/books/1.jsonld',
+    data: { key: :value }.to_json
   }
   req = http.request(req)
 
